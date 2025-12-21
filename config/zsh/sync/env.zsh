93e474b26c999b90dbea748a3db70e241aa00c3e
ujiuji1259 <suzzz428@gmail.com>
2025-12-21 14:31:46 +0900

    remove

--- config/zsh/sync/env.zsh
+++
@@ -1,9 +0,0 @@
-# History設定
-## history
-HISTSIZE=100000
-SAVEHIST=100000
-setopt share_history
-setopt inc_append_history
-export HISTFILE="$XDG_STATE_HOME/zsh_history"
-
-export PATH=$PATH:"/Applications/Visual Studio Code.app/Contents/Resources/app/bin"
--- scripts/setup.bash
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-# shellcheck source=./scripts/common.bash
-source "$(dirname "$0")/common.bash"
-/bin/bash "$CUR_DIR/setup-links.bash"
