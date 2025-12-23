ac6c4e8aefbc53d97bb0625c47a1989f5b312f5b
Shogo Ujiie <shogo-ujiie@m3.com>
2025-12-23 22:30:47 +0900

    add hidden files

--- configs/nvim/lua/plugins/oil.lua
+++ configs/nvim/lua/plugins/oil.lua
@@ -9,6 +9,14 @@ return {
     -- Top Pickers & Explorer
     { "<leader>o", function() require("oil").open() end, desc = "Oil File Explorer" },
   },
+  config = function ()
+    require("oil").setup({
+      view_options = {
+        -- Show files and directories that start with "."
+        show_hidden = true,
+      },
+    })
+  end,
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
--- configs/nvim/lua/plugins/picker.lua
+++ configs/nvim/lua/plugins/picker.lua
@@ -1,7 +1,10 @@
 return {
   "folke/snacks.nvim",
   opts = {
-    picker = {},
+    picker = {
+        hidden = true,
+        ignored = true,
+    },
     explorer = {},
   },
   keys = {
