dc5923a2336eadbe1ca45ffb0b2dd1f7d5087f14
ujiuji1259 <suzzz428@gmail.com>
2025-12-23 22:07:37 +0900

    add acceleraated-jk

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -3,3 +3,11 @@ require('options')
 require('keymaps')
 
 require("config.lazy")
+
+-- setup must be called before loading
+vim.cmd("colorscheme nightfox")
+
+-- setup accelerated-jk
+vim.api.nvim_set_keymap('n', 'j', '<Plug>(accelerated_jk_gj)', {})
+vim.api.nvim_set_keymap('n', 'k', '<Plug>(accelerated_jk_gk)', {})
+
