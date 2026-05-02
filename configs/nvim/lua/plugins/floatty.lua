a1d4c08755ee15c36a73741ad98717693a656766
ujiuji1259 <suzzz428@gmail.com>
2026-05-02 16:51:22 +0900

    add plugins

---
+++ configs/nvim/lua/plugins/floatty.lua
@@ -0,0 +1,11 @@
+return {
+    "ingur/floatty.nvim",
+    config = function()
+        -- initialize config
+        local term = require("floatty").setup({})
+
+        -- set toggle keybinds (supports v:count by default!)
+        vim.keymap.set('n', '<C-t>', function() term.toggle() end)
+        vim.keymap.set('t', '<C-t>', function() term.toggle() end)
+    end,
+}
---
+++ configs/nvim/lua/plugins/hop.lua
@@ -0,0 +1,12 @@
+return {
+    "smoka7/hop.nvim",
+    version = "*",
+    opts = {
+        keys = "etovxqpdygfblzhckisuran",
+    },
+    lazy = true,
+    keys = {
+        { "<leader>w", ":HopWord<CR>" },
+        { "<leader>s", ":HopChar2<CR>" },
+    },
+}
